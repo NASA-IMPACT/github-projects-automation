96043c5f81cd6d4c4960b91e35b7c82d58663010
Saurav <saurav@Sauravs-MacBook-Pro.local>
2023-06-20 16:06:52 -0500

    updated

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from github_graphql import get_viewer_login
+from dotenv import load_dotenv
 
-personal_access_token = "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"
+# Load environment variables from the .env file
+load_dotenv()
+
+import os
+
+# Access the token value
+personal_access_token = os.getenv("TOKEN_SECRET")
 
 viewer_login = get_viewer_login(personal_access_token)
 print(f"Authenticated as: {viewer_login}")
